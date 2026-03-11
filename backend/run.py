7339871e1a80ffb119299b6e9797d6387aa52ad8
Gordon Mo <Gordon62105@gmail.com>
2026-03-10 20:05:50 -0400

    Fixed backend run.py for windows compatibility

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,12 @@
+import asyncio
+import sys
+
 from app.app import app
-    
+
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    print("Applied WindowsSelectorEventLoopPolicy for compatibility on Windows.")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
